a91a32dddfad9bb9390dd3a700d3fc91d00bf6da
Suvadeep <suvadeepm0709@gmail.com>
2025-02-24 13:12:57 +0530

    mongodb connection working

--- Cart-mern-ts/backend/app.ts
+++ Cart-mern-ts/backend/app.ts
@@ -1,1 +1,14 @@
-console.log("Hello TypeScript with Node.js");
+import "dotenv/config"; // Load environment variables
+import express from "express";
+import cors from "cors";
+import path from "path";
+import mongoose from "mongoose";
+import connectDB from "./config/db.ts";
+
+const port = 5000;
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+  connectDB();
+});
